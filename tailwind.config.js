ea8abf3d73b5bcafe80ff10ead9ffacf4c709c85
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-11-19 01:25:44 +0700

    💄add: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {},
-      colors: {},
+      fontFamily: {
+        "Noto-Sans": ["Noto Sans Thai", "sans-serif"],
+        "IBM-Plex-Sans": ["IBM Plex Sans", "sans-serif"],
+      },
+      colors: {
+        navy: "#0A2468",
+        skyblue: "#28B6EF",
+        lightblue: "#3558F0",
+        lightgreen: "#11C277",
+      },
+      backgroundImage: {
+        hero: "url('/images/bangkok-map-blur.png')",
+      },
     },
   },
   plugins: [],
