705c2f58aefd1795eb866ae8723ab0e09da6133f
Just3rd <zombielandsaga12@gmail.com>
2023-11-19 23:29:01 +0700

    💄 Update SDGs component

--- src/components/Home/SDGs.tsx
+++ src/components/Home/SDGs.tsx
@@ -3,16 +3,16 @@ const SDGs = () => {
     <div className="bg-navy py-12">
       <div className="sm:flex justify-center items-center gap-x-10 p-6">
         {/* SDGs */}
-        <div className="text-white w-[500px]">
+        <div className="text-white w-[700px]">
           <h1 className="text-5xl font-bold py-4">SDGs คืออะไร?</h1>
           <p>
             เป้าหมายการพัฒนาที่ยั่งยืน (Sustainable Development Goals: SDGs) เป็นชุดเป้าหมายการพัฒนาระดับโลกหลังปี 2015
             ที่ได้รับการรับรองจาก 193 ประเทศสมาชิกขององค์การสหประชาชาติ เมื่อวันที่ 25 กันยายน ค.ศ. 2015
             ครอบคลุมช่วงระยะเวลาที่ต้องบรรลุภายใน 15 ปี
             ได้เป็นทิศทางการพัฒนาที่ทุกประเทศที่ต้องดำเนินการร่วมกันมาตั้งแต่ปีค.ศ. 2016 ไปจนถึงปี ค.ศ. 2030
-            โดยเอกสารที่ประเทศสมาชิกทั้งหมดลงนามรับรองเป็นพันธะสัญญานั้นเรียกว่า “Transforming Our World:
+            โดยเอกสารที่ประเทศสมาชิกทั้งหมดลงนามรับรองเป็นพันธะสัญญานั้นเรียกว่า “
             <a href="https://sdgs.un.org/2030agenda" target="_blank" className="underline">
-              the 2030 Agenda for Sustainable Development
+              Transforming Our World: the 2030 Agenda for Sustainable Development
             </a>
             ” หรือ “วาระการพัฒนาที่ยั่งยืน 2030” ฉะนั้นในบางโอกาส SDGs อาจถูกกล่าวถึงผ่านชื่ออื่นได้ทั้ง Agenda 2030
             หรือ Global Goals เป้าหมายการพัฒนาที่ยั่งยืน (Sustainable Development Goals: SDGs) มีทั้งหมด 17 เป้าหมาย
