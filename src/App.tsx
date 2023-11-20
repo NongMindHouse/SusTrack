341b87d192f7d17970a21c4a59435dc90dab073c
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-11-20 17:37:57 +0700

    ✨feat: NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import NotFound from "./pages/404";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 type Props = {};
 
 const Navbar: React.FC<Props> = ({}) => {
+  const { pathname } = useLocation();
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleNavbar = () => {
     setShowMenu(!showMenu);
   };
+
+  // setShowMenu false when change page
+  useEffect(() => {
+    setShowMenu(false);
+  }, [pathname]);
 
   const navbarList = [
     { name: "แผนที่ SDGs", path: "/map" },
@@ -23,38 +29,24 @@ const Navbar: React.FC<Props> = ({}) => {
         <Link to="/" className="pl-6 text-2xl text-white font-bold">
           SusTrack
         </Link>
+        {/* Bar3 */}
         <button
-          data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
+          onClick={toggleNavbar}
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden bg-gray-100 focus:outline-none focus:ring-2 focus:ring-lightblue"
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
+          {showMenu ? <img src="/icon/close.svg" /> : <img src="/icon/bar3.svg" />}
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+
+        {/* NavList */}
+        <div className={showMenu ? "w-full md:block md:w-auto" : "hidden w-full md:block md:w-auto"}>
           <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
             {navbarList.map((nav) => (
               <Link
                 className={
-                  window.location.pathname == nav.path
-                    ? "block py-4 px-4 text-white rounded-2xl md:p-0 bg-lightblue"
-                    : "block py-2 px-3 text-white rounded-2xl md:p-0 hover:bg-lightblue"
+                  pathname == nav.path
+                    ? "block py-1 px-4 text-white rounded-2xl bg-lightblue"
+                    : "block py-1 px-4 text-white rounded-2xl hover:bg-lightblue"
                 }
                 key={nav.path}
                 to={nav.path}
