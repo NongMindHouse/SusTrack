18f41b9e54a10d9561035901495ad3a5ee6374f1
Just3rd <zombielandsaga12@gmail.com>
2023-11-24 05:19:44 +0700

    💄 Add sdg number

--- src/components/Map/SDGFilter.tsx
+++ src/components/Map/SDGFilter.tsx
@@ -10,14 +10,27 @@ const SDGFilter: React.FC<Props> = ({ handleClick, selectedSDG }) => {
   return (
     <div className="bg-gray-100 mx-auto mb-5 p-5 rounded-lg flex flex-wrap">
       {sdgColorList.map((color, i: number) => (
-        <img
-          key={i}
-          src={`/images/sdgs/sdg-${i + 1}.svg`}
-          className={selectedSDG.includes(i) ? `sdg-block opacity-100` : "sdg-block"}
-          style={{ backgroundColor: color, opacity: 0.2 }}
-          alt={`SDG ${i}`}
-          onClick={() => handleClick(i + 1)}
-        />
+        <div className="relative hover:scale-[1.07] transition
+          duration-100 cursor-pointer"
+        >
+          <p className="absolute left-2 top-1 text-white font-bold text-[14px]">
+            {i+1}
+          </p>
+          <img
+            key={i}
+            src={`/images/sdgs/sdg-${i + 1}.svg`}
+            className={
+              selectedSDG.includes(i) ?
+              "opacity-100 block w-[64px] h-[64px] rounded-sm p-3 m-1" :
+              "block w-[64px] h-[64px] rounded-sm p-3 m-1"
+            }
+            // className="block w-[64px] h-[64px] rounded-sm p-3 m-1"
+            // style={{ backgroundColor: color, opacity: 0.2 }}
+            style={{ backgroundColor: color}}
+            alt={`SDG ${i}`}
+            onClick={() => handleClick(i + 1)}
+          />
+        </div>
       ))}
     </div>
   );
