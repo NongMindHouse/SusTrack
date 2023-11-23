a59559f22c449c637060364ad7d391e56ad02a0b
Just3rd <zombielandsaga12@gmail.com>
2023-11-24 05:01:36 +0700

    💄 Update ur for sidebar

---
+++ src/components/Map/ProjectCardMap.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import SDGList from "./SDGList";
+
+type Props = {
+    id: string;
+    title: string;
+    image: string;
+    desc: string;
+    sdgs:number[];
+};
+
+const ProjectCardMap: React.FC<Props> = ({ id, image, title, desc, sdgs }) => {
+  const navigate = useNavigate();
+  const Goto = () => {
+    navigate(`/project/${id}`);
+  };
+
+  return (
+  <div
+    className="bg-gray-300 rounded-lg cursor-pointer hover:scale-[1.02] h-[150px]
+    transition duration-100 shadow-sm hover:shadow-md flex gap-x-[5px] p-2"
+    onClick={Goto}
+  >
+      <div className="w-3/5 rounded-md overflow-hidden">
+        <img src={image} className="h-full w-full object-cover"/>
+      </div>
+      <div className="flex flex-col gap-y-2 p-3">
+        <p className="text-[16px] font-bold leading-5">{title}</p>
+        <p className="text-[14px] leading-4">{desc}</p>
+        <SDGList selectedSDG={sdgs}/>
+      </div>
+  </div>
+  );
+};
+
+export default ProjectCardMap;
--- src/components/Map/SDGList.tsx
+++ src/components/Map/SDGList.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { sdgColorList } from "@/utils/sdgColors";
 
-type Props = {};
+type Props = {
+  selectedSDG: number[];
+};
 
-const SDGList: React.FC<Props> = () => {
-  return <div>SDGList</div>;
+const SDGList: React.FC<Props> = ({selectedSDG}) => {
+  return (
+    <div className="flex gap-x-[3px]">
+      {selectedSDG.map((i:number) => (
+          <img src={`images/sdgs/sdg-${i}.svg`}
+          className="block w-[25px] h-[25px] rounded-sm p-[4px]"
+          style={{backgroundColor:sdgColorList[i-1]}}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default SDGList;
