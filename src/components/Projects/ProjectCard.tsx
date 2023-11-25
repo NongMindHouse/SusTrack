7734fa64083a3f60fe2896bb3517758341b7b2de
nut-somwang <82942709+guynut@users.noreply.github.com>
2023-11-25 19:57:01 +0700

    🍻Image cover fig Height

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -15,7 +15,9 @@ const ProjectCard: React.FC<Props> = ({ id, image, title, desc }) => {
       className="max-w-sm bg-gray-100 shadow rounded-xl hover:cursor-pointer hover:border-lightblue hover:shadow-md hover:scale-[1.01] duration-100"
       onClick={Goto}
     >
-      <img className="rounded-t-xl" src={image} alt="" />
+      <div className=" w-full h-40">
+        <img className="rounded-t-xl object-cover" src={image} alt="" />
+      </div>
       <div className="p-5">
         <h5 className="mb-2 text-xl font-bold tracking-tight text-black ">{title}</h5>
         <p className="mb-3 text-black ">{desc}</p>
